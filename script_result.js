c1ee62de73a5dfa290bba41670fcf41572dced52
Jenny <jenny369314@gmail.com>
2023-10-31 11:35:14 +0800

    各種修改+視覺回饋與特效的部分

---
+++ script_result.js
@@ -0,0 +1,7 @@
+$(document).ready(function() {
+    window.onload = function(){
+        $(".main").fadeIn(2000);
+        document.querySelector(".main").style.display = "flex";
+        document.querySelector(".main").style.justifyContent = "center";
+    }
+});
