62d2fc4177b379604b30caccca6919a761921cf4
Jenny <jenny369314@gmail.com>
2023-10-31 19:09:38 +0800

    上傳圖片 + 調整

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ $(document).ready(function() {
     window.onload = function(){
         $(".main").fadeIn(2000);
         document.querySelector(".main").style.display = "flex";
-        document.querySelector(".main").style.flexDirection = "column-reverse";
+        document.querySelector(".main").style.flexDirection = "column";
         document.querySelector(".main").style.alignItems = "center";
         document.querySelector(".main").style.justifyContent = "center";
         document.querySelector(".main").style.flexGrow = "1";
