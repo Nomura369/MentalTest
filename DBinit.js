913d5cf0597ddc339492acfca901ec72e848c01d
Jenny <jenny369314@gmail.com>
2023-12-16 22:01:50 +0800

    firebase初始化 + 開始頁按鈕樣式

---
+++ DBinit.js
@@ -0,0 +1,13 @@
+$(document).ready(function() {
+    //Firebase初始化
+    const firebaseConfig = {
+        apiKey: "AIzaSyCH5c_zQVDfln4wmzVb6Pr56y8UgD3NsHM",
+        authDomain: "mentaltest-1ceda.firebaseapp.com",
+        projectId: "mentaltest-1ceda",
+        storageBucket: "mentaltest-1ceda.appspot.com",
+        messagingSenderId: "684988781392",
+        appId: "1:684988781392:web:127966dd6653a89c165255"
+      };
+
+      let db = firebase.firestore(); //宣告資料庫物件
+});
